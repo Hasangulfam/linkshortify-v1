8ad7b471f3e5a7b1382d8390cecc9403c634ce8a
Hasangulfam <90946351+Hasangulfam@users.noreply.github.com>
2023-10-05 18:33:15 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $SOURCE_CODE ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/kevinnadar22/URL-Shortener-V2.git /URL-Shortener-V2
+  git clone https://github.com/Hasangulfam/linkshortify-v1.git /linkshortify-v1
 else
   echo "Cloning Custom Repo from $SOURCE_CODE "
-  git clone $SOURCE_CODE /URL-Shortener-V2
+  git clone $SOURCE_CODE /linkshortify-v1
 fi
-cd /URL-Shortener-V2
+cd /linkshortify-v1
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 -m main
